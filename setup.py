c802126f2ce13cb88e7f695b86484637840fd464
Michal Charemza <michal@charemza.name>
2019-06-15 19:08:16 +0100

    (release) v0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='lowhaio_aws_sigv4',
-    version='0.0.3',
+    version='0.0.4',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='AWS Signature Version 4 signing for lowhaio',
